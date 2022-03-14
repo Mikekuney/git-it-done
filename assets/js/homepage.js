04a45097a437fc00dac8b0b1c31a0715b779ed79
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-03-14 12:03:39 -0400

    added function json response

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-}
+var getUserRepos = function(user) {
+    // format the GitHub API URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the apiUrl
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+console.log("outside");
+
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
 
 getUserRepos();
