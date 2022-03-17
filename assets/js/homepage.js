5d409af9592e9d9778bc101948b9fea133483aa9
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-03-16 21:18:24 -0400

    call api functions when buttons are clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector('#language-buttons');
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -24,6 +25,20 @@ var getUserRepos = function(user) {
     });
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            }); 
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
 var response = fetch("https://api.github.com/users/octocat/repos");
 //console.log(response);
 
@@ -37,6 +52,16 @@ var formSubmitHandler = function(event) {
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute('data-language');
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = '';
     }
 };
 
@@ -91,3 +116,4 @@ var displayRepos = function(repos, searchTerm) {
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
